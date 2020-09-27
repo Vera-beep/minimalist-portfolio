e41b791b37bd984b8a952e0be279ed3b49a1467f
Max H <mh@carplounge.de>
2020-09-27 14:13:30 +0200

    Add ig link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,6 +13,7 @@
             <nav class="nav right">
                 <g-link class="nav__link" :to="{ name: 'home' }">WORK</g-link>
                 <g-link class="nav__link" to="/contact">SAY HI!</g-link>
+                <g-link class="nav__link" to="https://www.instagram.com/efef_v/">IG</g-link>
             </nav>
         </div>
     </header>
